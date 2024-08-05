02dbb4b6f2c5968e7d72495936d7944e4ae23082
sky.ren <rensky0742@gmail.com>
2024-08-05 21:39:08 +0800

    feat: add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={inter.className}>
+                <Header />
                 <div className='flex min-h-screen w-full flex-col'>{children}</div>
             </body>
         </html>
